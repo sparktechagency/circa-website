3f079d905d0a9b29fddef808f6830c341096c1a8
mdbayzid2025 <ssmdbayzid23@gmail.com>
2026-03-28 11:32:02 +0600

    c u

--- src/app/(creator)/(creator-dashboard)/creator-profile/page.tsx
+++ src/app/(creator)/(creator-dashboard)/creator-profile/page.tsx
@@ -1,10 +1,12 @@
 import CreatorProfile from '@/components/ui/creator/Profile'
+import FanProfile from '@/components/ui/creator/Profile/FanProfile'
 import React from 'react'
 
 const page = () => {
   return (
     <div className="">      
-      <CreatorProfile />
+      {/* <CreatorProfile /> */}
+      <FanProfile />
     </div>
 
   )
